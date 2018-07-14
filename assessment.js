ebb9fb1f9e38bcbf5ffc58fa92bd6984a4db1723
William Albright <ping@ewilli.am>
2018-07-14 01:18:23 -0700

    pass tests!

--- assessment.js
+++ assessment.js
@@ -7,7 +7,7 @@ After writing your function uncomment the matching function reference at the bot
 // 1. Write a function called helloWorld that returns the string 'Hello World!'.
 
 function helloWorld() {
-
+  return "Hello World!";
 }
 
 /*
@@ -23,8 +23,16 @@ function helloWorld() {
              lambdaSchool(8); // returns 8
 */
 
-function lambdaSchool() {
-
+function lambdaSchool(num) {
+  if ((num % 3 === 0) && (num % 5 === 0)) {
+    return "Lambda School";
+  } else if (num % 3 == 0) {
+    return "Lambda";
+  } else if (num % 5 == 0) {
+    return "School";
+  } else {
+    return num;
+  }
 }
 
 /*
@@ -38,8 +46,8 @@ function lambdaSchool() {
              longestString(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
 */
 
-function longestString() {
-
+function longestString(strs) {
+  return strs.reduce(function (a, b) { return a.length >= b.length ? a : b; });
 }
 
 /*
@@ -63,8 +71,20 @@ function longestString() {
              computeUserAverageAge(users); // returns 62 (This number is rounded up from 61.6666)
 */
 
-function computeUserAverageAge() {
+function computeUserAverageAge(users) {
+  var ageTotal = 0;
+  var count = 0
 
+  for (var user in users) {
+    if (users.hasOwnProperty(user)) {
+      var this_user = users[user];
+      ageTotal += this_user.age
+      count++
+    }
+  }
+
+  var averageAge = (ageTotal / count).toFixed(0);
+  return parseInt(averageAge);
 }
 
 module.exports = {
